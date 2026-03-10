0149597a87f384c49683e7ab5420e0ab7a705ee1
north <northa6217550@gmail.com>
2026-03-11 00:39:21 +0800

    異動配置

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/lastZ-power/', // 用於 GitHub Pages 部署
+  base: '/', // 用於 GitHub Pages 部署
   build: {
     outDir: 'dist',
     sourcemap: false,
